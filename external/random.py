4eef0f2c68c4924ab6a37c6158fa9cd0ea82d25e
David Vielhuber <david@vielhuber.de>
2025-06-30 16:09:10 +0200

    - Add type hints to function parameters and return types for improved code clarity. - Update existing functions with detailed type annotations for input parameters. - Refactor functions' return types to enhance type safety and consistency.

--- external/random.py
+++ external/random.py
@@ -1,17 +1,18 @@
 import json
 import random
+from typing import List, Any
 
 
 class RandomModel:
 
     name = "random"
 
-    def model_train_model(filename, filename_model, train_window, train_horizon):
+    def model_train_model(filename: str, filename_model: str, train_window: int, train_horizon: int) -> None:
         # pseudo save
         with open(filename_model, "w", encoding="utf-8") as f:
             json.dump([], f)
 
-    def model_buy_sell_order(X_df, filename_model, trade_confidence):
+    def model_buy_sell_order(X_df: Any, filename_model: str, trade_confidence: int) -> float:
         # Zufällige Wahrscheinlichkeit zwischen 0 und 1
         prob = random.uniform(0, 1)
         upper = trade_confidence / 100  # alles drüber ist BUY
@@ -22,7 +23,7 @@ class RandomModel:
             return 0
         return 0.5
 
-    def model_run_fulltest(filename_model, X_test, trade_confidence):
+    def model_run_fulltest(filename_model: str, X_test: List[List[float]], trade_confidence: int) -> List[float]:
         prognosen = []
         upper = trade_confidence / 100  # alles drüber ist BUY
         lower = 1 - upper  # alles drunter ist SELL
